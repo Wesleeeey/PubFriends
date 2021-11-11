c1bb91f2c2b00c5edae326982099160a9e218ce1
Wesley <71722269+Wesleeeey@users.noreply.github.com>
2021-11-11 05:57:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const { token, msg } = require('./config.json');
 
+let count = 1;
+
 client.on('ready', () => {
-    let count = 1;
     client.user.friends.forEach(member => {
         member.send(msg).catch(console.error).then(() => {
             count++
